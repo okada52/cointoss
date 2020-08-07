20b08978e8f685c597d0bdc1839692892af8685b
Honami Okada <s1270111@u-aizu.ac.jp>
2020-08-07 13:54:03 +0900

    User name added

--- HeadsOrTails.c
+++ HeadsOrTails.c
@@ -1,23 +1,8 @@
 #include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
 int main(){
-  
-  int countH=0,countT =0;
-  printf("Tossing a coin...\n");
-  srand((unsigned)time(NULL));
-    for(int i=0;i<3;i++){
-    int c;
-    c = rand() % 2 + 1;
-    if(c==1){
-       printf("Round %d: Heads\n",i+1);
-      countH++;
-    }else{
-       printf("Round %d: Tails\n",i+1);
-      countT++;
-    }
-      
-  }
-  printf("Heads: %d, Tails: %d\n",countH, countT);
+	char name[100];
+	printf("Who are you?\n");
+	scanf("%s",name);
+	printf("Hello, %s!\n",name);
   return 0;
   }
